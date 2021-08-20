092f3a317762992a7a66b55ac2a5668233addb47
Claudimar <claudimards@gmail.com>
2021-08-19 22:13:39 -0300

    cod aula 02

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import Layout from "./components/layout";
+import Profile from "./components/profile";
 
-function App() {
+const App = () => {
   return (
     <main className="App">
       <Layout>
-        <div>
-          <img
-            src="https://avatars.githubusercontent.com/u/39865182?v=4"
-            alt="Avatar claudimards"
-          />
-          <h1>Claudimar</h1>
-          <h3>Username: </h3>
-          <span>claudimards</span>
-
-          <div>
-            <div>
-              <h4>Followers</h4>
-              <span>5000000</span>
-            </div>
-            <div>
-              <h4>Starreds</h4>
-              <span>5000000</span>
-            </div>
-            <div>
-              <h4>Following</h4>
-              <span>5000000</span>
-            </div>
-          </div>
-        </div>
+        <Profile />
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
     </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/profile/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import * as S from "./styled";
+
+const Profile = () => {
+  return (
+    <S.Wrapper>
+      <S.WrapperImage
+        src="https://avatars.githubusercontent.com/u/39865182?v=4"
+        alt="Avatar claudimards"
+      />
+      <S.WrapperInfoUser>
+        <div>
+          <h1>Claudimar Bezerra</h1>
+          <S.WrapperUserName>
+            <h3>Username: </h3>
+            <span>claudimards</span>
+          </S.WrapperUserName>
+        </div>
+
+        <S.WrapperStatusCount>
+          <div>
+            <h4>Followers</h4>
+            <span>5000000</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>5000000</span>
+          </div>
+          <div>
+            <h4>Following</h4>
+            <span>5000000</span>
+          </div>
+        </S.WrapperStatusCount>
+      </S.WrapperInfoUser>
+    </S.Wrapper>
+  );
+};
+
+export default Profile;
